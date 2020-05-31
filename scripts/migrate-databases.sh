cf73750c9ab6ec2cb677086c43ce695e5e9587bc
prabhakar79git <prabhakar.mca03@gmal.com>
2020-05-31 15:20:37 -0400

    Pipeline changes

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -9,7 +9,7 @@ service_key="${4:-flyway-migration-key}"
 
 function main() {
 
-    echo "Creating service key, if necessary ..."
+    echo "Creating service key, if necessary ."
 
     cf create-service-key $service_name $service_key > /dev/null
 
